c474153e85232d32d1d5653352bc7f4e23c86e1b
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-06-04 23:51:06 -0300

    loading

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  protected loading: boolean;
 
+  constructor() {
+    this.loading = true;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2300);
+  }
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome.component';
-
+import { LoadingModule } from 'src/app/components/loading/loading.module';
 
 @NgModule({
-  declarations: [
-    WelcomeComponent
-  ],
-  imports: [
-    CommonModule,
-    WelcomeRoutingModule
-  ]
+  declarations: [WelcomeComponent],
+  imports: [CommonModule, WelcomeRoutingModule, LoadingModule],
 })
-export class WelcomeModule { }
+export class WelcomeModule {}
