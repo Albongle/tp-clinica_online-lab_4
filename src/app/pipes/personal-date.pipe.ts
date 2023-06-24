82b70ac6ea05e90a4a63f8851bd62d54cc425bc9
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-06-24 20:42:52 -0300

    se le da semantica a las variables

--- src/app/pipes/personal-date.pipe.ts
+++ src/app/pipes/personal-date.pipe.ts
@@ -6,11 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PersonalDatePipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): string {
     const splitDate = value.split('/').reverse();
-    const date = new Date(
-      parseInt(splitDate[0]),
-      parseInt(splitDate[1]),
-      parseInt(splitDate[2])
-    );
+    const [year, month, day] = splitDate;
+    const date = new Date(parseInt(year), parseInt(month), parseInt(day));
     return `${date.getDay()} de ${date.toLocaleDateString('es-ES', {
       month: 'long',
     })}`;
