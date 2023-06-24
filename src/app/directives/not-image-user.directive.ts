e0e70433331ec1dda3de13c0cb4f38c203caafd5
u531677 <ANBongioanni@teco.com.ar>
2023-06-24 12:55:45 -0300

    rename directive

--- src/app/directives/not-image-user.directive.ts
+++ src/app/directives/not-image-user.directive.ts
@@ -1,10 +1,11 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[notImageUser]',
+  selector: '[appNotImageUser]',
 })
 export class NotimageUserDirective {
   constructor(private readonly image: ElementRef) {}
+
   @HostListener('error')
   onError(): void {
     this.image.nativeElement.src = '../../assets/images/user_default.png';
