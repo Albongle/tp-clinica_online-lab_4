565f6a766cff520a9d40061716460e7d15dc1871
Alejandro Bongioanni <60529636+Albongle@users.noreply.github.com>
2023-09-27 14:06:08 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: 'AIzaSyA__0h6YI1JBDh4XTmlC9GGhbYr_d2d_VQ',
-    authDomain: 'clinica-online-1fc45.firebaseapp.com',
-    projectId: 'clinica-online-1fc45',
-    storageBucket: 'clinica-online-1fc45.appspot.com',
-    messagingSenderId: '2201364971',
-    appId: '1:2201364971:web:c07362ce2f473e2f2611ae',
-  },
+    apiKey: ''
 };
